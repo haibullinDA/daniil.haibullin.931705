067471b0763d80e65181abfd89724828fbfa92d4
haibullinDA <66507087+haibullinDA@users.noreply.github.com>
2020-06-07 22:22:02 +0700

    initial commit

---
+++ Lab4/Lab4/Controllers/ControlsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class ControlsController : Controller
+    {
+        static ControlModel controls = new ControlModel();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TextBox()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TextBox(string TB)
+        {
+            ControlModel cm = new ControlModel
+            {
+                ControlElement = "Text Box",
+                Result = TB,
+                Name = "Text: "
+            };
+            return View("ResultControl", cm);
+        }
+
+        [HttpGet]
+        public IActionResult TextArea()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TextArea(string TA)
+        {
+            ControlModel cm = new ControlModel
+            {
+                ControlElement = "Text Area",
+                Result = TA,
+                Name = "Text: "
+            };
+            return View("ResultControl", cm);
+        }
+
+
+        [HttpGet]
+        public IActionResult CheckBox()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckBox(bool isSelected)
+        {
+            ControlModel cm = new ControlModel
+            {
+                ControlElement = "Check Box",
+                Name = "Selected"
+            };
+            if (isSelected == false)
+            {
+                cm.Result = "False";
+            }
+            else
+            {
+                cm.Result = "True";
+            }
+            return View("ResultControl", cm);
+        }
+
+        [HttpGet]
+        public IActionResult Radio()
+        {
+            ViewBag.Radio = controls.Radio;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save_Radio(string Radio)
+        {
+            controls.Radio = Radio;
+            ViewBag.Radio = controls.Radio;
+            return View("Radio");
+        }
+
+        [HttpGet]
+        public IActionResult DropDownList()
+        {
+            ViewBag.DropDownList = controls.DropDownList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save_DropDownList(string DropDownList)
+        {
+            controls.DropDownList = DropDownList;
+            ViewBag.DropDownList = controls.DropDownList;
+            return View("DropDownList");
+        }
+
+        [HttpGet]
+        public IActionResult ListBox()
+        {
+            ViewBag.ListBox = controls.ListBox;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save_ListBox(string ListBox)
+        {
+            controls.ListBox = ListBox;
+            ViewBag.ListBox = controls.ListBox;
+            return View("ListBox");
+        }
+    }
+}
---
+++ Lab4/Lab4/Models/ControlModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class ControlModel
+    {
+        public string Result { get; set; }
+        public string ControlElement { get; set; }
+        public string Name { get; set; }
+        public string Radio { get; set; } = null;
+        public string DropDownList { get; set; } = null;
+        public string ListBox { get; set; } = null;
+    }
+}
